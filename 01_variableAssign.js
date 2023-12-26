46ff2126df440e2770cb86aa173017aff9514574
bharatjadhavbj <bj007143@gmail.com>
2023-12-26 08:54:07 +0530

    done
    done assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,28 @@
+var myFullName; //Declaration
+myFullName="Bharat Jadhav";
+console.log("My First & Last Name is=",myFullName);
+myFullName="Bharat Ramchandra Jadhav";  //variable update 
+console.log("My Full Name :",myFullName);
+
+
+// assign 2
+var myLove="I love only JS";
+console.log("My love=",myLove);
+
+//assign 3
+
+var firstName="Bharat";
+var lastName="Jadhav";
+var age="28";
+var address="Sangola";
+var pinCode="413307";
+var gender="Male";
+var isMarried="Un Married";
+
+console.log("First Name :-",firstName);
+console.log("Last Name :-",lastName);
+console.log("Age :-",age);
+console.log("Address :-",address);
+console.log("Pin Code :-",pinCode);
+console.log("Gender :-",gender);
+console.log("Is Married :-",isMarried);
---
+++ helloWorld.js
@@ -0,0 +1,13 @@
+// console.log("Hello JS, i am Bharat");
+var num;  //veriable declaration
+num= "10"; //veriable initilisation
+console.log(num);
+var num="10";  //veriable decl and initi
+console.log(num)
+console.log("Bharat")
+var myName="bharat";
+console.log(myName);
+console.log("First Name-",myName);
+myName="Bharat Jadhav";  //variable update
+console.log("Full name :-",myName);
+
