aa4bf9f7160b2dd322cabeed9f4abe9b9e2dfe86
bharatjadhavbj <154484250+bharatjadhavbj@users.noreply.github.com>
2024-01-24 11:35:51 +0530

    DOM Assignment complete
    DOM Assignment complete

---
+++ dom/14_main.js
@@ -0,0 +1,9 @@
+
+console.log(`----------Changing Heading color =>----------`);
+const headingMain=document.querySelector(".mainHeading");
+headingMain.style.color='orange';
+
+console.log(`----------Changing Project Description =>-----------`);
+
+const projectDescribe = document.querySelector("#projectDes");
+projectDescribe.innerHTML = "Project Description : This project is mainly used for student management in college, where It help to manage student record like student registration,student update information and to see all his or her acaemic acivities like score card,attendence .";
